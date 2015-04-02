712d3842c94491fa3f98f8502f86475a975b0ff3
Antoine Hauck <antoine.hauck@bbv.ch>
2015-04-02 22:34:25 +0200

    - Initial commit
    - .gitignore
    - Chapter 15 async/await

---
+++ Chapter15_Async_Await/AwaitingCancellation.cs
@@ -0,0 +1,33 @@
+﻿namespace Chapter15_Async_Await
+{
+    using System;
+    using System.ComponentModel;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    [Description("Cancellation of Tasks")]
+    class AwaitingCancellation
+    {
+        static void Main()
+        {
+            var tokenSource = new CancellationTokenSource();
+            Task task = DelayFor30Seconds(tokenSource.Token);
+            tokenSource.CancelAfter(TimeSpan.FromSeconds(1));
+
+            try
+            {
+                task.Wait();
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("Caught {0}", e.InnerExceptions[0]);
+            }
+        }
+
+        static async Task DelayFor30Seconds(CancellationToken token)
+        {
+            Console.WriteLine("Waiting for 30 seconds ...");
+            await Task.Delay(TimeSpan.FromSeconds(30), token);
+        }
+    }
+}
---
+++ Chapter15_Async_Await/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Chapter15_Async_Await
+{
+  using System;
+
+  using MiscUtil;
+
+  class Program
+  {
+    [STAThread]
+    static void Main(string[] args)
+    {
+      ApplicationChooser.Run(typeof(Program), args);
+    }
+  }
+}
