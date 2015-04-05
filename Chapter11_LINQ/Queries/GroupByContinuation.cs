1d0c06e83bff2c2cdba17387c9dbe24bee389c82
Antoine Hauck <antoine.hauck@bbv.ch>
2015-04-05 19:35:26 +0200

    Continuing a grouping

---
+++ Chapter11_LINQ/Queries/GroupByContinuation.cs
@@ -0,0 +1,26 @@
+﻿namespace Chapter11_LINQ.Queries
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq;
+
+    using Chapter11_LINQ.Model;
+
+    [Description("Listing 11.19")]
+    public class GroupByContinuation
+    {
+        static void Main()
+        {
+            var query = from defect in SampleData.AllDefects
+                        where defect.AssignedTo != null
+                        group defect by defect.AssignedTo
+                        into groupedDefects
+                        select new { Name = groupedDefects.Key, Count = groupedDefects.Count() };
+
+            foreach (var entry in query)
+            {
+                Console.WriteLine("{0}: {1}", entry.Name, entry.Count);
+            }
+        }
+    }
+}
