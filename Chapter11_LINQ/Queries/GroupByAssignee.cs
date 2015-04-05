3fe5d9712487901a9383f8f089ac706ae575005e
Antoine Hauck <antoine.hauck@bbv.ch>
2015-04-05 19:03:02 +0200

    GroupJoin and GroupBy

---
+++ Chapter11_LINQ/Queries/GroupByAssignee.cs
@@ -0,0 +1,28 @@
+﻿namespace Chapter11_LINQ.Queries
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq;
+
+    using Chapter11_LINQ.Model;
+
+    [Description("Listing 11.17")]
+    public class GroupByAssignee
+    {
+        static void Main()
+        {
+            var query = from defect in SampleData.AllDefects
+                        where defect.AssignedTo != null
+                        group defect by defect.AssignedTo;
+
+            foreach (var entry in query)
+            {
+                Console.WriteLine(entry.Key);
+                foreach (var defect in entry)
+                {
+                    Console.WriteLine("  [{0}] {1}", defect.Severity, defect.Summary);
+                }
+            }
+        }
+    }
+}
---
+++ Chapter11_LINQ/Queries/GroupJoinAssignee.cs
@@ -0,0 +1,28 @@
+﻿namespace Chapter11_LINQ.Queries
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq;
+
+    using Chapter11_LINQ.Model;
+
+    [Description("Listing 11.17a with Group Join")]
+    public class GroupJoinAssignee
+    {
+        static void Main()
+        {
+            var query = from user in SampleData.AllUsers
+                        join defect in SampleData.AllDefects on user equals defect.AssignedTo into joinedDefects
+                        select new { Name = user.Name, Defects = joinedDefects };
+
+            foreach (var entry in query)
+            {
+                Console.WriteLine(entry.Name);
+                foreach (var defect in entry.Defects)
+                {
+                    Console.WriteLine("  [{0}] {1}", defect.Severity, defect.Summary);
+                }
+            }
+        }
+    }
+}
