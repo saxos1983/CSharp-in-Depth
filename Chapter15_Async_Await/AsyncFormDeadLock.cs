eff742f4163eca114b0ef1d90bc51536734fce8b
Antoine Hauck <antoine.hauck@bbv.ch>
2015-04-16 21:54:52 +0200

    Await, Wait (DeadLock), ConfigureAwait example

--- Chapter15_Async_Await/AsyncFormDeadLock.cs
+++ Chapter15_Async_Await/AsyncFormDeadLock.cs
@@ -15,11 +15,21 @@ namespace Chapter15_Async_Await
     [Description("Await, Wait (DeadLock), ConfigureAwait")]
     public partial class AsyncFormDeadLock : Form
     {
+        /// <summary>
+        /// Have a look at http://blog.stephencleary.com/2012/07/dont-block-on-async-code.html
+        /// to understand the different scenarios how you can initiate/prevent a DeadLock.
+        /// </summary>
         public AsyncFormDeadLock()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// This will cause a DeadLock because the method waits synchronously
+        /// for the task to complete. The task will try to marshal the continuation
+        /// back to the calling context (which is the UI thread) but the UI thread is
+        /// already waiting for the task completion --> DeadLock.
+        /// </summary>
         private void btnWaitDeadlock_Click(object sender, EventArgs e)
         {
             var task = this.DoLongRunningTask();
@@ -27,6 +37,9 @@ namespace Chapter15_Async_Await
             labelStatusValue.Text = task.Result;
         }
 
+        /// <summary>
+        /// Because the task is being awaited, the UI will remain responsive.
+        /// </summary>
         private async void btnAwaitNoDeadlock_Click(object sender, EventArgs e)
         {
             var task = this.DoLongRunningTask();
@@ -34,6 +47,12 @@ namespace Chapter15_Async_Await
             labelStatusValue.Text = result;
         }
 
+        /// <summary>
+        /// The UI will not remain responsive during the execution of the task,
+        /// but no DeadLock will occur because the continuation will not be marshalled
+        /// back to the original context (which is the UI thread) due to the
+        /// ConfigureAwait(false).
+        /// </summary>
         private void btnConfigureAwaitNoDeadlock_Click(object sender, EventArgs e)
         {
             var task = this.DoLongRunningTaskConfigureAwait();
@@ -44,14 +63,14 @@ namespace Chapter15_Async_Await
         private async Task<string> DoLongRunningTask()
         {
             labelStatusValue.Text = "Executing...";
-            await Task.Delay(TimeSpan.FromSeconds(1));
+            await Task.Delay(TimeSpan.FromSeconds(3));
             return "Finished";
         }
         
         private async Task<string> DoLongRunningTaskConfigureAwait()
         {
             labelStatusValue.Text = "Executing...";
-            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            await Task.Delay(TimeSpan.FromSeconds(3)).ConfigureAwait(false);
             return "Finished";
         }
 
